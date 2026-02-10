64ca56fbb69117aef3228ac775a44572f05d193a
obuleshkumarpula966 <obuleshkumarpula966@gmail.com>
2026-02-10 13:15:46 +0530

    Add files via upload

---
+++ diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+ int row,col;
+ printf("Enter the no of rows\n");
+ scanf("%d",&row);
+ printf("Enter the no of columns\n");
+ scanf("%d",&col );
+ for (int i=1;i<=row;i++)
+ {
+ for (int j=1;j<=col;j++)
+  {
+   if (i==1||i==row||j==1||j==col||i==j)
+   {
+   printf("*");
+   }
+   else
+   {
+   printf(" ");
+   }
+  }
+  printf("\n");
+ }
+}
